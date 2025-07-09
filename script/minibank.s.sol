620221c30a5d14b1261857b7291173812697dc02
paps <lateeftosin99@gmail.com>
2025-07-09 15:39:02 +0100

    updated script for deployment

--- script/minibank.s.sol
+++ script/minibank.s.sol
@@ -1,4 +1,13 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
-import "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {MiniBank} from "../src/MiniBank.sol";
+contract deployMiniBAnk is Script {
+    function run() external returns (MiniBank) {
+        vm.startBroadcast();
+        MiniBank miniBank = new MiniBank();
+        vm.stopBroadcast();
+
+        return miniBank;
+    }
+}
